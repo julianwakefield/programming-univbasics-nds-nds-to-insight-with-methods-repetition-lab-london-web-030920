a464c88510b389b83516f6c8e0c202671071d86e
Julian Wakefield <julianwakefield@outlook.com>
2020-03-06 13:53:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,62 +3,58 @@ require_relative './directors_database'
 require 'pp'
 require 'pry'
 
-# def directors_totals(source)
-#   result = {}
-#     director_index = 0
-#    while director_index < source.size do
-#       director = source[director_index]
-#     result[director[:name]] = gross_for_director(director)
-#     director_index += 1
-#    end
-#   result
-# end
-#
-# def gross_for_director(d)
-#    total = 0
-#    index = 0
-#
-#   while index < d[:movies].length do
-#     total += d[:movies][index][:worldwide_gross]
-#      index += 1
-#    end
-#
-#   total
-#  end
-#
-#  def list_of_directors(source)
-# #   #Write this implementation
-#   counter = 0
-#   list_of_directors = []
-#
-#
-#   while counter < source.length do
-#    director_names = source[counter][:name]
-#     list_of_directors << director_names
-#
-#     counter += 1
-#   end
-#  list_of_directors
-#
-# end
+def directors_totals(source)
+  result = {}
+    director_index = 0
+   while director_index < source.size do
+      director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+   end
+  result
+end
+
+def gross_for_director(d)
+   total = 0
+   index = 0
+
+  while index < d[:movies].length do
+    total += d[:movies][index][:worldwide_gross]
+     index += 1
+   end
+
+  total
+ end
+
+ def list_of_directors(source)
+#   #Write this implementation
+  counter = 0
+  list_of_directors = []
+
+
+  while counter < source.length do
+   director_names = source[counter][:name]
+    list_of_directors << director_names
+
+    counter += 1
+  end
+ list_of_directors
+
+end
 
 def total_gross(source)
-#    result = {}
-    # counter = 0
-    # while counter < source.length do
-    #  director_name = source[counter][:name]
-    #   puts director_name
-    #   counter += 1
-    # end
-
-#    i = 0
-#    while i < source.length do
-#      gross = source[:name][:movies][i][:worldwide_gross]
-#      result += gross
-#      counter += 1
-#    end
-
-
+    result = 0
+    counter = 0
+    while counter < source.length do
+       film = source[counter][:movies]
+       i = 0
+       while i < film.length do
+         result += film[i][:worldwide_gross] 
+         i += 1
+       end
+       counter += 1
+   end
+   result
 end
 
 # Write this implementation
